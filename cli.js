213e2f68a3a1d4d748b9a6e7489ce7378dda7618
Sophie Jiang <sjiang13@email.unc.edu>
2023-04-08 13:17:01 -0400

    help message

--- cli.js
+++ cli.js
@@ -8,13 +8,13 @@ const args = minimist(process.argv.slice(2));
 
 
 if (args.h) {
-	console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
 			-h            Show this help message and exit.
 			-n, -s        Latitude: N positive; S negative.
 			-e, -w        Longitude: E positive; W negative.
 			-z            Time zone: uses tz.guess() from moment-timezone by default.
 			-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-			-j            Echo pretty JSON from open-meteo API and exit.");
+			-j            Echo pretty JSON from open-meteo API and exit.`);
 	process.exit(0);
 }
 
